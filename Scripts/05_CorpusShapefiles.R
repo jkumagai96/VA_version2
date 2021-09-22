e3b54c049a75a40c964332534e328f973b5567b1
Joy Kumagai <49419490+jkumagai96@users.noreply.github.com>
2021-09-22 14:38:58 +0200

    + regions and subregions maps

--- Scripts/05_CorpusShapefiles.R
+++ Scripts/05_CorpusShapefiles.R
@@ -1,5 +1,5 @@
 # Joy Kumagai (joy.kumagai@senckenberg.de)
-# Date: September 21st 2021
+# Date: September 22nd 2021
 # Create four maps with the CORPUS dataset, indicators, and variables for before 1990, 1990-1999, 2000 - 2009, 2010 to now
 # Values Assessment 
 
@@ -55,8 +55,8 @@ plot1 <- ggplot(poly) + # Names 1 all studies
         axis.title = element_blank(),
         legend.position = "bottom") +
   scale_fill_gradient(
-    low = "#267311",
-    high = "#ECF649",
+    low = "#F7FCB9",
+    high = "#006837",
     space = "Lab",
     na.value = "grey",
     aesthetics = "fill",
@@ -85,8 +85,8 @@ plot1_log <- ggplot(poly) + # Names 2 log all studies
   annotate("text", x = -8000000, y = 9500000, label = "180°W", size = 3) +
   annotate("text", x = 8000000, y = 9500000, label = "180°E", size = 3) +
   scale_fill_gradient(
-    low = "#267311",
-    high = "#ECF649",
+    low = "#F7FCB9",
+    high = "#006837",
     space = "Lab",
     na.value = "grey",
     aesthetics = "fill",
@@ -124,8 +124,8 @@ plot2 <- ggplot(poly) + # Names 2 all studies
   annotate("text", x = -8000000, y = 9500000, label = "180°W", size = 3) +
   annotate("text", x = 8000000, y = 9500000, label = "180°E", size = 3) +
   scale_fill_gradient(
-    low = "#08519C",
-    high = "#DEEBF7",
+    low = "#DEEBF7",
+    high = "#08519C",
     space = "Lab",
     na.value = "grey",
     aesthetics = "fill",
@@ -158,8 +158,8 @@ plot2_log <- ggplot(poly) + # Names 2 log all studies
   annotate("text", x = -8000000, y = 9500000, label = "180°W", size = 3) +
   annotate("text", x = 8000000, y = 9500000, label = "180°E", size = 3) +
   scale_fill_gradient(
-    low = "#08519C",
-    high = "#DEEBF7",
+    low = "#DEEBF7",
+    high = "#08519C",
     space = "Lab",
     na.value = "grey",
     aesthetics = "fill",
@@ -180,8 +180,234 @@ png("Outputs/Maps/Names2_percountry.png", width = 8, height = 8, units = "in", r
 plot_grid(plot2, plot2_log, labels = "auto", ncol = 1)
 dev.off()
 
-##### Graph by region #####
+##### Prepare data by region #####
+# Download data
+inborutils::download_zenodo("10.5281/zenodo.3928281", path = "Data/ipbes_regions_subregions/", quiet = FALSE)
+unzip("Data/ipbes_regions_subregions/ipbes_regions_subregions_shape_1.1.zip", exdir = "Data/ipbes_regions_subregions")
 
+# Load needed data 
+regions <- st_read("Data/ipbes_regions_subregions/IPBES_regions_Subregions2.shp")
+corpus <- read_excel("Data/IPBES_VA_Uptake_Corpus_06May20_GEONames_TS_16June20.xlsx", sheet = 1)
+
+# Dissolve data by ipbes region
+#data_region <- regions %>% # this dissolves the data by region
+#  mutate(n = 1) %>% 
+#  dplyr::group_by(Region) %>% 
+#  dplyr::summarise(count_n = sum(n)) %>% 
+#  sf::st_cast() 
+#st_write(data_region, "Data/ipbes_regions_subregions/region.shp")
+data_region <- st_read("Data/ipbes_regions_subregions/region.shp")
+
+# Count number of times per region (NAMES 1)
+names1_region_counts <- corpus %>% 
+  mutate(x = strsplit(as.character(Region_TI_AB_DE_ID), ", ")) %>% 
+  unnest(x) %>% 
+  count(x, sort = TRUE) %>% 
+  rename("Region" = "x")
+
+n1_region_poly <- left_join(data_region, names1_region_counts) %>% 
+  st_wrap_dateline() %>% 
+  st_transform(robin_crs)
+
+# Count number of times per region (NAMES 2)
+names2_region_counts <- corpus %>%
+  mutate(x = strsplit(as.character(Region_CI_FU_FX), ", ")) %>% 
+  unnest(x) %>% 
+  count(x, sort = TRUE) %>% 
+  rename("Region" = "x")
+
+n2_region_poly <- left_join(data_region, names2_region_counts, by = "Region") %>% 
+  st_wrap_dateline() %>% 
+  st_transform(robin_crs)
+  
+##### Prepare data by subregion #####  
+# Dissolve data by ipbes subregion
+#data_subregion <- regions %>% # this dissolves the data by subregion
+#  mutate(n = 1) %>% 
+#  dplyr::group_by(Sub_Region) %>% 
+#  dplyr::summarise(count_n = sum(n)) %>% 
+#  sf::st_cast()
+#st_write(data_subregion, "Data/ipbes_regions_subregions/subregion.shp")
+data_subregion <- st_read("Data/ipbes_regions_subregions/subregion.shp")
+
+# Count number of times per sub region (NAMES 1)
+names1_subregion_counts <- corpus %>% 
+  mutate(x = strsplit(as.character(Subregion_TI_AB_DE_ID), ", ")) %>% 
+  unnest(x) %>% 
+  count(x, sort = TRUE) %>% 
+  rename("Sub_Region" = "x")
+
+fix <- names1_subregion_counts %>% # Within the data western and central europe are separated, I added them together to match the regions within the polygon data
+  filter(Sub_Region == "Western Europe" | Sub_Region == "Central Europe") 
+fix <- sum(fix$n)
+
+names1_subregion_counts <- names1_subregion_counts %>% 
+  add_row(tibble_row(Sub_Region = "Central and Western Europe", n = fix))
+
+# final join
+n1_subregion_poly <- left_join(data_subregion, names1_subregion_counts, by = "Sub_Region") %>%
+  st_wrap_dateline() %>% 
+  st_transform(robin_crs)
+
+# Count number of times per subregion (NAMES 2)
+names2_subregion_counts <- corpus %>%
+  mutate(x = strsplit(as.character(Subregion_CI_FU_FX), ", ")) %>% 
+  unnest(x) %>% 
+  count(x, sort = TRUE) %>% 
+  rename("Sub_Region" = "x")
+
+# Within the data western and central europe are separated, I added them together to match the regions within the polygon data
+fix <- names2_subregion_counts %>% 
+  filter(Sub_Region == "Western Europe" | Sub_Region == "Central Europe")
+fix <- sum(fix$n)
+
+names2_subregion_counts <- names2_subregion_counts %>% 
+  add_row(tibble_row(Sub_Region = "Central and Western Europe", n = fix))
+
+# final join
+n2_subregion_poly <- left_join(data_subregion, names2_subregion_counts, by = "Sub_Region") %>% 
+  st_wrap_dateline() %>% 
+  st_transform(robin_crs)
+
+##### Graph data by region and sub-region ######
+
+# Subregion names 1
+plot3 <- ggplot(n1_subregion_poly) + 
+  geom_sf(data = grid, 
+          colour = "gray60",
+          linetype = "dashed") +
+  geom_sf(aes(fill = n, colour = NULL)) +
+  annotate("text", x = -18000000, y = 0, label = "0°", size = 3) +
+  annotate("text", x = -18000000, y = 3200000, label = "30° N", size = 3) +
+  annotate("text", x = -15500000, y = 6200000, label = "60° N", size = 3) +
+  annotate("text", x = -18000000, y = -3200000, label = "30° S", size = 3) +
+  annotate("text", x = -15500000, y = -6200000, label = "60° S", size = 3) +
+  annotate("text", x = 0, y = 9500000, label = "0°", size = 3) +
+  annotate("text", x = -3000000, y = 9500000, label = "60°W", size = 3) +
+  annotate("text", x = 3000000, y = 9500000, label = "60°E", size = 3) +
+  annotate("text", x = -8000000, y = 9500000, label = "180°W", size = 3) +
+  annotate("text", x = 8000000, y = 9500000, label = "180°E", size = 3) +
+  scale_fill_gradient(
+    low = "#F7FCB9",
+    high = "#006837",
+    space = "Lab",
+    na.value = "grey",
+    aesthetics = "fill",
+    n.breaks = 4, 
+    guide = guide_colorbar(title.position = "top",
+                           title.hjust = .5,
+                           barwidth = 10, 
+                           barheight = 0.5
+    )) +
+  labs(fill = "Density of Studies") +
+  theme(panel.background = element_blank(), 
+        axis.text.x = element_text(size = 12),
+        axis.title = element_blank(),
+        legend.position = "bottom")
+
+# Export 
+png("Outputs/Maps/Names1_persubregion.png", width = 8, height = 5, units = "in", res = 600)
+plot3
+dev.off()
+
+# Subregion names 2
+plot4 <- ggplot(n2_subregion_poly) + 
+  geom_sf(data = grid, 
+          colour = "gray60",
+          linetype = "dashed") +
+  geom_sf(aes(fill = n, colour = NULL)) +
+  annotate("text", x = -18000000, y = 0, label = "0°", size = 3) +
+  annotate("text", x = -18000000, y = 3200000, label = "30° N", size = 3) +
+  annotate("text", x = -15500000, y = 6200000, label = "60° N", size = 3) +
+  annotate("text", x = -18000000, y = -3200000, label = "30° S", size = 3) +
+  annotate("text", x = -15500000, y = -6200000, label = "60° S", size = 3) +
+  annotate("text", x = 0, y = 9500000, label = "0°", size = 3) +
+  annotate("text", x = -3000000, y = 9500000, label = "60°W", size = 3) +
+  annotate("text", x = 3000000, y = 9500000, label = "60°E", size = 3) +
+  annotate("text", x = -8000000, y = 9500000, label = "180°W", size = 3) +
+  annotate("text", x = 8000000, y = 9500000, label = "180°E", size = 3) +
+  scale_fill_gradient(
+    low = "#DEEBF7",
+    high = "#08519C",
+    space = "Lab",
+    na.value = "grey",
+    aesthetics = "fill",
+    n.breaks = 4, 
+    guide = guide_colorbar(title.position = "top",
+                           title.hjust = .5,
+                           barwidth = 10, 
+                           barheight = 0.5
+    )) +
+  labs(fill = "Density of Institutions") +
+  theme(panel.background = element_blank(), 
+        axis.text.x = element_text(size = 12),
+        axis.title = element_blank(),
+        legend.position = "bottom")
+
+# Export 
+png("Outputs/Maps/Names2_persubregion.png", width = 8, height = 5, units = "in", res = 600)
+plot4
+dev.off()
+
+# Plot regions by names 1
+n1_region_poly$n <- as.factor(n1_region_poly$n)
+
+plot5 <- ggplot(n1_region_poly) + 
+  geom_sf(data = grid, 
+          colour = "gray60",
+          linetype = "dashed") +
+  geom_sf(aes(fill = n, colour = NULL)) +
+  annotate("text", x = -18000000, y = 0, label = "0°", size = 3) +
+  annotate("text", x = -18000000, y = 3200000, label = "30° N", size = 3) +
+  annotate("text", x = -15500000, y = 6200000, label = "60° N", size = 3) +
+  annotate("text", x = -18000000, y = -3200000, label = "30° S", size = 3) +
+  annotate("text", x = -15500000, y = -6200000, label = "60° S", size = 3) +
+  annotate("text", x = 0, y = 9500000, label = "0°", size = 3) +
+  annotate("text", x = -3000000, y = 9500000, label = "60°W", size = 3) +
+  annotate("text", x = 3000000, y = 9500000, label = "60°E", size = 3) +
+  annotate("text", x = -8000000, y = 9500000, label = "180°W", size = 3) +
+  annotate("text", x = 8000000, y = 9500000, label = "180°E", size = 3) +
+  scale_fill_manual(values = c("#F7FCB9", "#ADDD8E", "#238443", "#006837", "grey")) +
+  labs(fill = "Density of Studies") +
+  theme(panel.background = element_blank(), 
+        axis.text.x = element_text(size = 12),
+        axis.title = element_blank(),
+        legend.position = "bottom")
+
+# Export 
+png("Outputs/Maps/Names1_perregion.png", width = 8, height = 5, units = "in", res = 600)
+plot5
+dev.off()
+
+# Plot regions by names 2
+n2_region_poly$n <- as.factor(n2_region_poly$n)
+
+plot6 <- ggplot(n2_region_poly) + 
+  geom_sf(data = grid, 
+          colour = "gray60",
+          linetype = "dashed") +
+  geom_sf(aes(fill = n, colour = NULL)) +
+  annotate("text", x = -18000000, y = 0, label = "0°", size = 3) +
+  annotate("text", x = -18000000, y = 3200000, label = "30° N", size = 3) +
+  annotate("text", x = -15500000, y = 6200000, label = "60° N", size = 3) +
+  annotate("text", x = -18000000, y = -3200000, label = "30° S", size = 3) +
+  annotate("text", x = -15500000, y = -6200000, label = "60° S", size = 3) +
+  annotate("text", x = 0, y = 9500000, label = "0°", size = 3) +
+  annotate("text", x = -3000000, y = 9500000, label = "60°W", size = 3) +
+  annotate("text", x = 3000000, y = 9500000, label = "60°E", size = 3) +
+  annotate("text", x = -8000000, y = 9500000, label = "180°W", size = 3) +
+  annotate("text", x = 8000000, y = 9500000, label = "180°E", size = 3) +
+  scale_fill_manual(values = c("#DEEBF7", "#9ECAE1", "#4292C6", "#08519C", "grey")) +
+  labs(fill = "Density of Institutions") +
+  theme(panel.background = element_blank(), 
+        axis.text.x = element_text(size = 12),
+        axis.title = element_blank(),
+        legend.position = "bottom")
+
+# Export 
+png("Outputs/Maps/Names2_perregion.png", width = 8, height = 5, units = "in", res = 600)
+plot6
+dev.off()
 
 ##### Graph by year #####
 
@@ -192,3 +418,15 @@ dev.off()
 # 2000 - 2009
 
 # 2010 - 2020
+
+# Green even spaces
+scale_fill_manual(values = c("#F7FCB9", "#ADDD8E", "#41AB5D", "#006837", "grey"))
+
+# Blue non even spaces
+scale_fill_manual(values = c("#DEEBF7", "#6BAED6", "#2171B5", "#08519C", "grey"))
+
+# Green non even spaces (names 1)
+scale_fill_manual(values = c("#F7FCB9", "#ADDD8E", "#238443", "#006837", "grey"))
+
+# Blue even spaces (names 2)
+scale_fill_manual(values = c("#DEEBF7", "#9ECAE1", "#4292C6", "#08519C", "grey"))
